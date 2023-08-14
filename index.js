9385567f6aabb55107b39d26aebebe1d177fd606
jrdnwlkr <walker.jordan.m@gmail.com>
2023-08-14 17:43:56 -0500

    wrapped inquirer.prompt in userPrompts function

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+function userPrompts() {
 inquirer.prompt([
     {
         type: "input",
         name: "text",
-        message: "Select three (3) characters."
+        message: "Enter three (3) characters."
     },
     {
         type: "input",
@@ -16,13 +17,20 @@ inquirer.prompt([
         type: "list",
         name: "shape",
         message: "Choose a shape.",
-        choices: ["Circle", "Square", "Triangle"], 
+        choices: ["Circle", "Square", "Triangle"]
     },
     {
         type: "input",
         name: "shape-color",
         message: "Enter a color keyword or hexidecimal for the shape color."
     }
-]).then(userInput)
+])
+.then(answers => {
+    console.log(answers);
+})
+.catch(error => {
+    console.error('Error:', error);
+})
+}
 
-init()
+userPrompts();
